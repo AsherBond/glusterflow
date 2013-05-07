a3ea8bf2ffa1c76cd4317516b53e0d6189f55c8a
Justin Clift <jclift@redhat.com>
2013-05-07 10:11:46 +0100

    Started hooking in the gRaphaël code

--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+
+from ui.models import Flowdata
 
 def index(request):
-    return HttpResponse("Glusterflow UI index page")
+    all_flows = Flowdata.objects.order_by('-start_time')[:5]
+    template = loader.get_template('ui/index.html')
+    context = Context({
+        'all_flows': all_flows,
+    })
+    return HttpResponse(template.render(context))
