519627af848b6f2803ca02f0beca4b0f4a65658c
Justin Clift <jclift@redhat.com>
2013-05-08 04:53:04 +0100

    Started displaying the data better
    Still super early stages

--- ui/views.py
+++ ui/views.py
@@ -5,42 +5,38 @@ from datetime import datetime, timedelta
 from ui.models import Flowdata
 
 def index(request):
+    # Set up initial values
     all_flows = Flowdata.objects.all()
+    delta = timedelta(minutes=20)
+    rightnow = datetime.now()
+    num_slices = 12
 
     # Create the initial time slices
-    rightnow = datetime.now()
-    slice1 = rightnow - timedelta(minutes=20)
-    slice2 = slice1 - timedelta(minutes=20)
-    slice3 = slice2 - timedelta(minutes=20)
-    slice4 = slice3 - timedelta(minutes=20)
-    slice5 = slice4 - timedelta(minutes=20)
-    slice6 = slice5 - timedelta(minutes=20)
+    slices = []
+    slices.append(rightnow - delta)
+    for x in range(1, num_slices):
+        slices.append(slices[x-1] - delta)
 
     # Slice the flow data into time slices
-    flow1 = all_flows.filter(start_time__gte=slice1)
-    flow2 = all_flows.filter(start_time__gte=slice2).filter(start_time__lt=slice1)
-    flow3 = all_flows.filter(start_time__gte=slice3).filter(start_time__lt=slice2)
-    flow4 = all_flows.filter(start_time__gte=slice4).filter(start_time__lt=slice3)
-    flow5 = all_flows.filter(start_time__gte=slice5).filter(start_time__lt=slice4)
-    flow6 = all_flows.filter(start_time__gte=slice6).filter(start_time__lt=slice5)
+    flows = []
+    flows.append(all_flows.filter(start_time__gte=slices[0]))
+    for x in range(1, num_slices):
+        flows.append(all_flows.filter(start_time__gte=slices[x]).filter(start_time__lt=slices[x-1]))
 
     # Count the number of flows in each time slice
-    count1 = flow1.count()
-    count2 = flow2.count()
-    count3 = flow3.count()
-    count4 = flow4.count()
-    count5 = flow5.count()
-    count6 = flow6.count()
+    counts = []
+    for x in range(num_slices):
+      counts.append(flows[x].count())
 
     # Render and return the view
     template = loader.get_template('ui/index.html')
     context = Context({
         'all_flows': all_flows,
-        'count1': count1,
-        'count2': count2,
-        'count3': count3,
-        'count4': count4,
-        'count5': count5,
-        'count6': count6,
+        'rightnow': rightnow,
+        'num_slices': num_slices,
+        'delta': delta,
+        'slices': slices,
+        'flows': flows,
+        'counts': counts,
     })
     return HttpResponse(template.render(context))
