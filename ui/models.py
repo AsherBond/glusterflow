b43164daaa295b26100007a47b0fa96b39ed8fca
Justin Clift <jclift@redhat.com>
2013-05-10 22:22:07 +0100

    Added note about models.py needing index creation added

--- ui/models.py
+++ ui/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# This could really use index creation here, as they make a big
+# difference.  (I've been manually adding them using psql so far)
 class Flowdata(models.Model):
     server = models.CharField(max_length=80)
     protocol = models.CharField(max_length=6)
