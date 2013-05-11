d8678c3afade0d3ab24aa6ff9a93dd7a40d5d54d
Justin Clift <jclift@redhat.com>
2013-05-11 05:02:32 +0100

    Added initial indexes to the flowdata table

--- ui/models.py
+++ ui/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# This could really use index creation here, as they make a big
-# difference.  (I've been manually adding them using psql so far)
 class Flowdata(models.Model):
     server = models.CharField(max_length=80)
     protocol = models.CharField(max_length=6)
     operation = models.CharField(max_length=12)
-    filename = models.CharField(max_length=1024)
-    start_time = models.DateTimeField('operation start time')
+    filename = models.CharField(max_length=1024, db_index=True)
+    start_time = models.DateTimeField('operation start time', db_index=True)
