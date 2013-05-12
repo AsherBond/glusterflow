5380541e2a3c65d2049df5b433c1798061710300
Justin Clift <jclift@redhat.com>
2013-05-12 07:11:28 +0100

    Refactored message sending code into a common function

--- json/glusterflowclient.py
+++ json/glusterflowclient.py
@@ -13,8 +13,46 @@ This is to enable intelligent diagnostics and reporting cluster wide.
 # This needs to be changed into a .vol file option
 glusterflow_json_server = ('192.168.1.68', 13373)
 
+# Protocol string to send
+# This also needs to be changed into a .vol file option
+protocol = 'nfs'
+
 # Debug on or off
 debug = 0
+
+# Work out the hostname
+this_host = str(socket.getfqdn())
+
+
+def send_message(operation, file_name):
+
+    # Get timestamp
+    start_time = datetime.now()
+
+    # Create connection to GlusterFlow server
+    global msg
+    try:
+        msg = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        msg.connect(glusterflow_json_server)
+
+    except socket.error, e:
+        # Something went wrong when creating the connection
+        print 'ERROR: socket error occurred: ', e
+
+    else:
+        # Create GlusterFlow message to send
+        # It would be good to also include the hostname of the client too later on (if possible)
+        gf_message = {'server': this_host, 'protocol': protocol, 'operation': operation, 'file': file_name, 'start':str(start_time)}
+
+        # Send message to GlusterFlow server
+        json_message = json.dumps(gf_message)
+        try:
+            msg.sendall(json_message)
+        except socket.error, e:
+            print 'ERROR: Error received at msg.sendall() time: ', e
+        else:
+            msg.close()
+
 
 class xlator (Translator):
 
@@ -33,32 +71,8 @@ class xlator (Translator):
 #        print "loc->gfid : ", repr(loc.contents.gfid)
 #        print "loc->pargfid : ", repr(loc.contents.pargfid)
 
-        # Get timestamp
-        start_time = datetime.now()
-
-        # Create connection to GlusterFlow server
-        global msg
-        try:
-            msg = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            msg.connect(glusterflow_json_server)
-
-        except socket.error, e:
-            # Something went wrong when creating the connection
-            print 'ERROR: socket error occurred: ', e
-
-        else:
-            # Create GlusterFlow message to send
-            # It would be good to also include the hostname of the client too later on (if possible)
-            gf_message = {'server': str(self.hostname), 'protocol':'nfs', 'operation':'lookup', 'file':loc.contents.path, 'start':str(start_time)}
-
-            # Send message to GlusterFlow server
-            json_message = json.dumps(gf_message)
-            try:
-                msg.sendall(json_message)
-            except socket.error, e:
-                print 'ERROR: Error received at msg.sendall() time: ', e
-            else:
-                msg.close()
+        # Send GlusterFlow JSON message to collector
+        send_message('lookup', loc.contents.path)
 
         # Continue on to the next translator
         dl.wind_lookup(frame,POINTER(xlator_t)(),loc,xdata)
@@ -81,32 +95,8 @@ class xlator (Translator):
 
     def create_fop (self, frame, this, loc, flags, mode, umask, fd, xdata):
 
-        # Get timestamp
-        start_time = datetime.now()
-
-        # Create connection to GlusterFlow server
-        global msg
-        try:
-            msg = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            msg.connect(glusterflow_json_server)
-
-        except socket.error, e:
-            # Something went wrong when creating the connection
-            print 'ERROR: socket error occurred: ', e
-
-        else:
-            # Create GlusterFlow message to send
-            # It would be good to also include the hostname of the client too later on (if possible)
-            gf_message = {'server': str(self.hostname), 'protocol':'nfs', 'operation':'create', 'file':loc.contents.path, 'start':str(start_time)}
-
-            # Send message to GlusterFlow server
-            json_message = json.dumps(gf_message)
-            try:
-                msg.sendall(json_message)
-            except socket.error, e:
-                print 'ERROR: Error received at msg.sendall() time: ', e
-            else:
-                msg.close()
+        # Send GlusterFlow JSON message to collector
+        send_message('create', loc.contents.path)
 
         # Continue on to the next translator
         dl.wind_create(frame,POINTER(xlator_t)(),loc,flags,mode,umask,fd,xdata)
